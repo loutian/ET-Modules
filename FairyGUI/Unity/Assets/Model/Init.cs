0e824eaed6f0992999b6efa5c0a2117c48922532
gaoj <gaoj@mail.51.com>
2018-09-25 14:52:43 +0800

    解决在ET4.0下报错问题， 并针对4.0的目录结构做出调整

--- FairyGUI/Unity/Assets/Model/Init.cs
+++ FairyGUI/Unity/Assets/Model/Init.cs
@@ -1,23 +1,23 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
+using Google.Protobuf;
 using UnityEngine;
 
 namespace ETModel
 {
 	public class Init : MonoBehaviour
 	{
-		private readonly OneThreadSynchronizationContext contex = new OneThreadSynchronizationContext();
-
 		private async void Start()
 		{
 			try
-			{
-				if (Application.unityVersion != "2017.1.3p2")
+			{ 
+				if (!Application.unityVersion.StartsWith("2017.4"))
 				{
-				    Log.Warning($"请使用Unity2017.1.3p2, 下载地址:\n https://beta.unity3d.com/download/744dab055778/UnityDownloadAssistant-2017.1.3p2.exe?_ga=2.42497696.443074145.1521714954-1119432033.1499739574");
-                }
+					Log.Error($"新人请使用Unity2017.4版本,减少跑demo遇到的问题! 下载地址:\n https://unity3d.com/cn/unity/qa/lts-releases?_ga=2.227583646.282345691.1536717255-1119432033.1499739574");
+				}
 
-				SynchronizationContext.SetSynchronizationContext(this.contex);
+				SynchronizationContext.SetSynchronizationContext(OneThreadSynchronizationContext.Instance);
 
 				DontDestroyOnLoad(gameObject);
 				Game.EventSystem.Add(DLLType.Model, typeof(Init).Assembly);
@@ -28,7 +28,7 @@ namespace ETModel
 				Game.Scene.AddComponent<PlayerComponent>();
 				Game.Scene.AddComponent<UnitComponent>();
 				Game.Scene.AddComponent<ClientFrameComponent>();
-				//Game.Scene.AddComponent<UIComponent>();
+				// Game.Scene.AddComponent<UIComponent>();
 
 				// 下载ab包
 				await BundleHelper.DownloadBundle();
@@ -42,13 +42,7 @@ namespace ETModel
 				Game.Scene.AddComponent<OpcodeTypeComponent>();
 				Game.Scene.AddComponent<MessageDispatherComponent>();
 
-
-
-                //加载行为树
-                Log.Debug("开始加载行为树");
-                Game.Scene.AddComponent<BeahaviacComponent>();
-
-                //新Ui
+				//新Ui
                 Log.Debug("加载UI框架");
 			    Game.Scene.AddComponent<UIManagerComponent>();
 
@@ -64,7 +58,7 @@ namespace ETModel
 
 		private void Update()
 		{
-			this.contex.Update();
+			OneThreadSynchronizationContext.Instance.Update();
 			Game.Hotfix.Update?.Invoke();
 			Game.EventSystem.Update();
 		}
